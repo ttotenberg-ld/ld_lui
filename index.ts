1cb535817dee34bcbee98ffc87cffe6f1e14c127
Tom Totenberg <ttotenberg@launchdarkly.com>
2023-07-14 09:24:54 -0500

    removed hello tool

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import { ToolBox } from "raylu";
-import { HelloTool } from "./src/hello_tool";
 import { CreateBooleanFlag } from "./src/flag_tool";
 
 var tb = new ToolBox()
-tb.setTools([new HelloTool(), new CreateBooleanFlag()])
+tb.setTools([new CreateBooleanFlag()])
 
 module.exports = { tb };
